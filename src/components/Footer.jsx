2fd0b1da9c1dfae414b48d496846b672f128e91a
Abhyuday <abhyudaygupta2244@gmail.com>
2022-12-08 21:21:45 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/abhyuday-gupta-53939416a/"
+            target={'blank'}
+          >
+            Linkedin
+          </a>
+          <a href="https://github.com/Abhyuday-Gupta" target={'blank'}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
